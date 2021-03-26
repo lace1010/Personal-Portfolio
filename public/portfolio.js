0e121ac85cf267b21a2ed1829d00a7cd8cb5331d
Hunter Lacefield <lace1010@gmail.com>
2021-03-27 02:22:19 +0800

    Deploying app

---
+++ public/portfolio.js
@@ -0,0 +1,11 @@
+$(function () {
+  $("#emailClick").click(function () {
+    alert("Email me at: lace1010@gmail.com");
+  });
+});
+
+$(function () {
+  $("#phoneClick").click(function () {
+    alert("Cellphone Number: +65-8511-0149");
+  });
+});
