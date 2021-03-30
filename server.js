a620dc306cbb3c5ffbfcbe576c0f2a057079fd4d
Hunter Lacefield <lace1010@gmail.com>
2021-03-30 11:21:45 +0800

    added font awesome and google fonts to helmet csp

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ app.use(
     dnsPrefetchControl: false,
     contentSecurityPolicy: {
       directives: {
-        defaultSrc: ["'self'"],
+        // for fontAwesome and google fonts console.log said we need to add these links to defaultrc to allow indiviual icons and fonts
+        defaultSrc: [
+          "'self'",
+          "http://fonts.gstatic.com/s/notoserifsc/",
+          "https://use.fontawesome.com/releases/v5.8.1/webfonts/",
+        ],
         "img-src": ["'self'", "https://i.ibb.co/"], // Need this to have images shown (all images are on imgbb.com in one album)
         // following tow lines allow loading of scripts and CSS from your server only using self. jquery link is to connect with jquery and font awesome link is for font awesome obviously.
         scriptSrc: ["'self'", "https://code.jquery.com/jquery-3.1.1.js"],
         styleSrc: [
           "'self'",
+          "http://fonts.googleapis.com/css",
           "https://use.fontawesome.com/releases/v5.8.1/css/all.css",
         ],
         // need to allow fontawesome still...
